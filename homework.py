34b00829316fb542e1a0b6077457b429c8dab31a
unknown <poho1234569@gmail.com>
2020-10-21 23:39:02 -0700

    finally done

--- homework.py
+++ homework.py
@@ -1,2 +1,12 @@
-number = len(input('Enter a number: '))
-print(number)
+number = float(input('Enter a number: '))
+count = 0
+if number > 0:
+    number /= 10
+    count +=1
+    while number > 1:
+        number /= 10
+        count +=1
+        if 0 < number < 1:
+            number +=0
+            count +=0
+print(count)
