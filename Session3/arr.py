8f97df051ea62dcdbc0b31f6e1f2a418b260fa3e
unknown <poho1234569@gmail.com>
2020-10-29 02:00:15 -0700

    news

--- Session3/arr.py
+++ Session3/arr.py
@@ -18,3 +18,16 @@ for i in range(n - 1):
     fib.append(arr)
 result = fib[n]
 print(result)
+
+
+number = int(input('Enter a number: '))
+arr = []
+for i in range(number):
+    list = []
+    for j in range(i+1):
+        if j == 0 or j == i:
+            list.append(1)
+        else:
+            list.append(arr[i-1][j-1] + arr[i-1][j])
+    arr.append(list)
+    print(list)
